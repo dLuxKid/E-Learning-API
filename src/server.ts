bf640da7d065356b167f48a52fe0267ab9ce1c54
dLuxKid <adetunjimarvellous09@gmail.com>
2025-04-25 14:15:13 +0100

    fixed env ish

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
 import mongoose, { Error } from "mongoose";
 import app from "./app";
+
+dotenv.config({ path: "./.env" });
 
 const port = process.env.PORT;
 const DB = process.env.DB as string;
